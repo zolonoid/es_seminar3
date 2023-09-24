b82bb16073774b356864befd13494122fb418f1b
John Doe <jd@mail.web>
2023-09-24 13:43:26 +0300

    completed

---
+++ task1.js
@@ -0,0 +1,36 @@
+// Реализуйте класс Employee (сотрудник), который имеет следующие свойства и методы:
+// Свойство name (имя) - строка, имя сотрудника.
+// Метод displayInfo() - выводит информацию о сотруднике (имя) в консоль.
+
+// Реализуйте класс Manager (менеджер), который наследует класс Employee и имеет дополнительное свойство и метод:
+// Свойство department (отдел) - строка, отдел, в котором работает менеджер.
+// Метод displayInfo() - переопределяет метод displayInfo() родительского класса и выводит информацию о менеджере (имя и отдел).
+
+"use strict";
+
+class Employee {
+    constructor(name) {
+        this.name = name;
+    }
+
+    displayInfo() {
+        console.log(`Name: ${this.name}`);
+    }
+}
+
+class Manager extends Employee {
+    constructor(name, department) {
+        super(name);
+        this.department = department;
+    }
+
+    displayInfo() {
+        super.displayInfo();
+        console.log(`Department: ${this.department}`);
+    }
+}
+
+const employee = new Employee("John Smith");
+employee.displayInfo();
+const manager = new Manager("Jane Doe", "Sales");
+manager.displayInfo();
---
+++ task2.js
@@ -0,0 +1,42 @@
+// Реализуйте класс Product (товар), который имеет следующие свойства и методы:
+// Свойство name - название товара.
+// Свойство price - цена товара.
+// Свойство quantity - количество товара.
+
+// Реализуйте класс Order (заказ), который имеет следующие свойства и методы:
+// Свойство id (номер заказа) - число, уникальный номер заказа.
+// Свойство products (продукты) - массив, содержащий список продуктов в заказе.
+// Метод addProduct(product) - принимает объект класса Product и добавляет его в список продуктов заказа.
+// Метод getTotalPrice() - возвращает общую стоимость заказа, основанную на ценах продуктов.
+
+"use strict";
+
+class Product {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+}
+
+class Order {
+    products = [];
+
+    constructor(orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    addProduct(product) {
+        this.products.push(product);
+    }
+
+    getTotalPrice() {
+        return this.products.reduce((acc, cur) => acc + cur.price, 0);
+    }
+}
+
+const order = new Order(12345);
+const product1 = new Product("Phone", 500);
+order.addProduct(product1);
+const product2 = new Product("Headphones", 100);
+order.addProduct(product2);
+console.log(order.getTotalPrice());
